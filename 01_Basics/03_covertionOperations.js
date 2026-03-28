b53dc43783bda53c3b3670897c2c071729cfe72c
GURUKIRAN C S <157163749+Gurukiran603@users.noreply.github.com>
2026-03-28 19:20:57 +0000

    03_covertionoperation

---
+++ 01_Basics/03_covertionOperations.js
@@ -0,0 +1,39 @@
+// let score = "33abc"
+
+// const {score} = req.
+// console.log(typeof score); // string
+// console.log(typeof(score)); // string
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber);  //NaN
+
+
+// "33" => 33
+// "abc" => NaN
+// true =>1; false => 0
+
+// let isLoggedIn =true
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);  //true
+
+// let isLoggedIn = ""
+
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);  //false
+
+
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "guru" => true
+
+
+let someNUmber = 33
+
+let stringNumber = String(someNUmber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+

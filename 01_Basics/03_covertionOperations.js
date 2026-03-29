73890244de6cd9816168a807a6fcb4bcb61f4ca7
GURUKIRAN C S <157163749+Gurukiran603@users.noreply.github.com>
2026-03-29 11:52:02 +0000

    js_

--- 01_Basics/03_covertionOperations.js
+++ 01_Basics/03_covertionOperations.js
@@ -31,9 +31,76 @@
 // "guru" => true
 
 
-let someNUmber = 33
+// let someNUmber = 33
 
-let stringNumber = String(someNUmber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNUmber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*****************  Operations **************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "guru"
+// let str3 = str1+ str2
+// console.log(str3);  // hello guru
+
+// console.log("1"+2); //12
+// console.log(1+"2");  // 12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2");  //32
+
+// console.log((3+4) * 5 % 2);
+
+//  console.log(+true); // 1
+
+//  console.log(true+); // error
+
+// console.log(+"");
+
+// let num1,num2,num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// gameCounter++; //101 prefix 
+// console.log(gameCounter); 
+
+
+/*Undefined	 -Throw a TypeError exception.
+Null   -	Throw a TypeError exception.
+Boolean- 	Return 1n if prim is true and 0n if prim is false.
+BigInt	 -Return prim.
+Number	-Throw a TypeError exception.
+String	
+1. Let n be StringToBigInt(prim).
+2. If n is undefined, throw a SyntaxError exception.
+3. Return n.
+Symbol	Throw a TypeError exception. */
+
+ 
+ let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
 
---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,46 @@
+// primitve 
+
+//7 types : string , Number , boolean , null (ekdum khali) ,
+//               undefined ,symbol , BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isloggedIn = false 
+// const outSideTemp = null 
+// let userEmail;  // undefined 
+
+// const id = symbol('123') //unique
+// const anotherid = symbol('123') 
+
+// console.log(id == anotherid);  // false  
+
+// const bigNumber = 123456789023456789n // bigint 
+// console.log(bigNumber);
+
+
+
+// Reference (non primitive) 
+
+//array , objects , Functions 
+
+const heros = ["shaktimAN","IRONMAN"];
+console.log(typeof heros);
+
+
+let myObject={
+    name:"guru",
+    age : "21",
+}
+console.log(typeof myObject);
+
+
+
+const myFunction = function(){
+    console.log("gurukiran c s");
+}
+console.log(typeof(myFunction));
+
+ 
+
+// callbyvalue and callbyReference
